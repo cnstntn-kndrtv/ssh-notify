5c506f1812e7883155a67673180ab1e37b1f510e
cnstntn <cnstntn@blackbox>
2020-01-22 12:05:19 +0300

    init

--- README.md
+++ README.md
@@ -1,15 +1,24 @@
 # ssh-notify
   
+
+1. create Slack app and **incoming webhook** https://slack.com/intl/en-ru/help/articles/115005265063-Incoming-WebHooks-for-Slack?eu_nc=1  
+
+2. edit `./ssh-notify/notify.sh`  
+   change url and channel for Slack notifications:  
+    url="<YOUR_SLACK_INCOMING_WEBHOOK>"  
+    channel="#general"  
+
+3. 
 ```sh
 sudo cp -r ./ssh-notify /usr/local/
 # make it executable
 sudo chmod +x /usr/local/ssh-notify/notify.sh
 ```
-  
-to `/etc/pam.d/sshd` add this line:  
+
+4. to `/etc/pam.d/sshd` add this line:  
 ```
 session optional pam_exec.so seteuid /usr/local/ssh-notify/notify.sh
 ```
-  
-  
+5. try to ssh to your sever
+   
 icon from flaticon.com
--- ssh-notify/notify.sh
+++ ssh-notify/notify.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
 
-# popup
-export DISPLAY=:0
-notify-send "Security Warning" "SSH Connection Established with \"$USER\" $(echo $SSH_CONNECTION | sed 's/\(.*\) \(.*\) \(.*\) \(.*\)/from \1:\2 to \3:\4/')" \
--u critical -i /usr/local/alert/icon.png
+if [ "$PAM_TYPE" != "close_session" ]; then 
 
-# play sound
-beep -f 500 -l 100
-# play /usr/local/alert/sound.wav > /dev/null 2>&1
+    # popup
+    export DISPLAY=:0
+    notify-send "Security Warning" "SSH Connection Established with \"$USER\" $(echo $SSH_CONNECTION | sed 's/\(.*\) \(.*\) \(.*\) \(.*\)/from \1:\2 to \3:\4/')" \
+            -u critical -i /usr/local/alert/icon.png 
+
+    # Slack notification
+    url="<YOUR_SLACK_INCOMING_WEBHOOK>"
+    channel="#general"
+    
+    host="$(hostname)"
+    content="\"attachments\": [ { 
+        \"mrkdwn_in\": [\"text\", \"fallback\"], 
+        \"fallback\": \"SSH login: $PAM_USER connected to \`$host\`\", 
+        \"text\": \"SSH login to \`$host\`\", 
+        \"fields\": [ 
+            { 
+                \"title\": \"User\", 
+                \"value\": \"$PAM_USER\", 
+                \"short\": true 
+            }, { 
+                \"title\": \"IP Address\", 
+                \"value\": \"$PAM_RHOST\", 
+                \"short\": true } 
+        ], 
+        \"color\": \"#F35A00\" 
+    } ]"
+
+
+    curl -X POST --data-urlencode "payload={
+        \"channel\": \"$channel\", 
+        \"mrkdwn\": true, 
+        \"username\": \"SSH Notifications\", 
+        $content, \"icon_emoji\": 
+        \":inbox-tray:\"}" "$url"
+
+
+    # play sound
+    beep -f 500 -l 100 ;
+    # play /usr/local/alert/sound.wav > /dev/null 2>&1
+
+fi
